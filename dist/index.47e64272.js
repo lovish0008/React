e9056f87f8a7d6aaeb1a518e8ccc3fb24f2ecffa
Goyal <lovishgoyal@kpmg.com.sg>
2024-01-23 13:15:27 +0800

    ignite-1

---
+++ dist/index.47e64272.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hellow World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.47e64272.js.map
